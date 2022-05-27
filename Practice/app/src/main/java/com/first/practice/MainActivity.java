dff5f9706619e2d374a14fc723fd787707ee9f03
Shahzaib-A530 <bsef19a530@pucit.edu.pk>
2022-05-27 22:50:45 +0500

    Second  commit

--- Practice/app/src/main/java/com/first/practice/MainActivity.java
+++ Practice/app/src/main/java/com/first/practice/MainActivity.java
@@ -3,12 +3,26 @@ package com.first.practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button1);
+        txt = findViewById(R.id.textView1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("Muhammad (S.A.W) is the last Prophet of Allah");
+            }
+        });
     }
 }
